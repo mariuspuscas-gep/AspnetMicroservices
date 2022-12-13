5c520c305dbeacf5502fa7f815131b0dca5b357f
Marius Puscas <Marius.Puscas@gep.com>
2022-12-13 20:21:21 +0200

    Implemented catalog microservice

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetProducts();
+
+        Task<Product> GetProduct(string id);
+
+        Task<List<Product>> GetProductByName(string name);
+
+        Task<List<Product>> GetProductByCategory(string categoryName);
+
+        Task CreateProduct(Product product);
+
+        Task<bool> UpdateProduct(Product product);
+
+        Task<bool> DeleteProduct(string id);
+    }
+}
